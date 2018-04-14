21ea559b8ce5a31dc214ba14e0aff1309a5aaf58
nice_shot <shefferu@gmail.com>
2018-04-14 12:53:13 +0300

    Fixed some stuff with the 3d models rotation and created script for automatic doors in work building

---
+++ AiUnity/Assets/Scripts/SuzyLemonade/Agents/BusinessMan.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace SuzyLemonade {
+public class BusinessMan : MonoBehaviour {
+
+    public Transform target;
+    private NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        agent.SetDestination(target.position);
+	}
+}
+}
---
+++ AiUnity/Assets/Scripts/SuzyLemonade/WorkBuildingComponent.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkBuildingComponent : MonoBehaviour {
+
+    public GameObject rDoor;
+    public GameObject lDoor;
+    public float doorMovement = 1.25f;
+    public float doorSpeed = 8f;
+
+    private bool someoneNearEntrance = false;
+
+    void OnTriggerStay(Collider other) {
+        someoneNearEntrance = true;
+    }
+
+    void OnTriggerExit(Collider other) {
+        someoneNearEntrance = false;
+    }
+
+    void Update() {
+        if (someoneNearEntrance) {
+            OpenDoors();
+        } else {
+            CloseDoors();
+        }
+    }
+
+    void OpenDoors() {
+        MoveDoor(rDoor, doorMovement);
+        MoveDoor(lDoor, -doorMovement);
+    }
+
+    void CloseDoors() {
+        MoveDoor(rDoor, 0f);
+        MoveDoor(lDoor, 0f);
+    }
+
+    // Moves door on the X axis to the given position
+    void MoveDoor(GameObject door, float xPos) {
+        if (door.transform.localPosition.x == xPos) {
+            // Already at target position
+            return;
+        }
+
+        float currentX = Mathf.Lerp(door.transform.localPosition.x, xPos, doorSpeed * Time.deltaTime);
+        Vector3 movePos = new Vector3(currentX, 0f, 0f);
+        door.transform.localPosition = movePos;
+    }
+
+}
