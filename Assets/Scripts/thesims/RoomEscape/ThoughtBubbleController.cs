e17cc820cb0ee54e78c33e23a86d0970df4e6247
nice_shot <shefferu@gmail.com>
2018-08-08 08:45:43 +0300

    Added text bubble on 2D canvas so that it will allways be clear

---
+++ Assets/Scripts/thesims/RoomEscape/ThoughtBubbleController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+public class ThoughtBubbleController : MonoBehaviour {
+    public GameObject agent;
+    public float yChange;
+    public float xChange;
+
+    void Update() {
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(agent.transform.position);
+        screenPos.x += xChange;
+        screenPos.y += yChange;
+        transform.position = screenPos;
+    }
+}
