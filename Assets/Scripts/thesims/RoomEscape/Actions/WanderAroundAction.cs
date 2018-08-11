8c8b7af2942a1f5e663c3844bf0c3c63499aad3e
nice_shot <shefferu@gmail.com>
2018-08-11 17:53:37 +0300

    Wandering to random positions now

--- Assets/Scripts/thesims/RoomEscape/Actions/WanderAroundAction.cs
+++ Assets/Scripts/thesims/RoomEscape/Actions/WanderAroundAction.cs
@@ -1,6 +1,7 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
 
 using Infra.Collections;
 using Ai.Goap;
@@ -9,6 +10,7 @@ namespace RoomEscape {
     public class WanderAroundAction : ActionBase {
 
         public GameObjectPool wanderPool;
+        public float wanderingMaxDistance;
 
         void Awake() {
             AddEffect(States.WANDER, ModificationType.Set, true);    
@@ -16,19 +18,28 @@ namespace RoomEscape {
 
         public override List<IStateful> GetAllTargets(GoapAgent agent) {
             WanderPosition target = wanderPool.Borrow<WanderPosition>();
-            if (target == null) {
-                Debug.LogError("Can't get target!");
-                return base.GetAllTargets(agent);
-            } else {
-                Debug.Log("Got target: " + target);
-            }
-            target.transform.position = new Vector3(Random.Range(1f, 20f), 0f, Random.Range(1f, 8f));
+
+            target.transform.position = GetRandomPosition(agent.transform.position, wanderingMaxDistance);
+            
             return new List<IStateful> { target };
+        }
+
+        private Vector3 GetRandomPosition(Vector3 origin, float distance, int layermask = -1) {
+            Vector3 randomPosition = Random.insideUnitSphere * wanderingMaxDistance;
+            randomPosition += origin;
+
+            NavMeshHit navHit;
+            if (NavMesh.SamplePosition(randomPosition, out navHit, distance, layermask)) {
+                return navHit.position;
+            }
+
+            // Retries if there was a problem with the nav hit
+            return GetRandomPosition(origin, distance, layermask);
         }
 
         protected override bool OnDone(GoapAgent agent, WithContext context) {
             base.OnDone(agent, context);
-            // Maybe write a different pool
+            // TODO: Write a different gameobject pool or change this one to allow returning one object / just remove the pool.
             wanderPool.ReturnAll();
             //WanderPosition target = context.target as WanderPosition;
             //target.ReturnSelf();
