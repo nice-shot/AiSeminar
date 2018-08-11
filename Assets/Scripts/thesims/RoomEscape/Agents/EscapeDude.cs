5e89563ad8b1c8f49e12db583af6c3c189f8855c
nice_shot <shefferu@gmail.com>
2018-08-11 10:15:10 +0300

    Removed unused variables in escape dude

--- Assets/Scripts/thesims/RoomEscape/Agents/EscapeDude.cs
+++ Assets/Scripts/thesims/RoomEscape/Agents/EscapeDude.cs
@@ -4,15 +4,9 @@ using Ai.Goap;
 
 namespace RoomEscape {
     public class EscapeDude : Survivor {
-        private int goalIndex;
-
-        public bool hasEscaped;
-        public bool escapeRoute;
 
         protected override void Awake() {
             base.Awake();
-            hasEscaped = false;
-            escapeRoute = false;
             Goal mainGoal = new Goal();
             mainGoal[States.ESCAPED] = new Condition(CompareType.Equal, true);
             Goal secondaryGoal = new Goal();
