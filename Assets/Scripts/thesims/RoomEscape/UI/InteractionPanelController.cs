3f92259bd84e0929a5c2aca6e61dc6f3283701ca
nice-shot <shefferu@gmail.com>
2018-09-01 21:20:35 +0300

    added small helper method to keep interaction panel on screen

--- Assets/Scripts/thesims/RoomEscape/UI/InteractionPanelController.cs
+++ Assets/Scripts/thesims/RoomEscape/UI/InteractionPanelController.cs
@@ -12,17 +12,48 @@ public class InteractionPanelController : MonoBehaviour {
     public GameObject mainActionContainer;
     public GameObject secondaryActionContainer;
 
+    private RectTransform rectTransform;
+
     private void Awake() {
         if (instance == null) {
             instance = this;
         } else {
             Destroy(gameObject);
         }
+        rectTransform = GetComponent<RectTransform>();
         SetHidden(true);
     }
 
     private void Update() {
         transform.position = Input.mousePosition;
+        CheckBounds();
+    }
+
+    private void CheckBounds() {
+        Vector3 positionChange = new Vector3();
+
+        int maxHeight = Screen.height;
+        float bottom = transform.position.y;
+        float top = bottom + rectTransform.rect.height;
+
+        if (top > maxHeight) {
+            positionChange.y = maxHeight - top;
+        } else if (bottom < 0) {
+            positionChange.y = 0 - bottom;
+        }
+
+        int maxWidth = Screen.width;
+        float left = transform.position.x;
+        float right = left + rectTransform.rect.width;
+
+        if (right > maxWidth) {
+            positionChange.x = maxWidth - right;
+        } else if (left < 0) {
+            positionChange.x = 0 - left;
+        }
+
+        transform.position += positionChange;
+
     }
 
     public void SetText(string description, string main, string secondary) {
